da08f083c544f714fe8a30e708c01bb21b5d471d
martinaapalazzi <martinaapalazzi@gmail.com>
2024-02-27 15:24:54 +0100

    created columns

--- database/migrations/2024_02_27_134126_create_trains_table.php
+++ database/migrations/2024_02_27_134126_create_trains_table.php
@@ -17,22 +17,30 @@ return new class extends Migration
 
             // Azienda -> varchar(64) NOT NULL
             $table->string('company', 64);
+
             // Stazione di partenza -> varchar(64) NOT NULL
             $table->string('station_departures', 64);
+
             // Stazione di arrivo -> varchar(64) NOT NULL 
             $table->string('station_arrivals', 64);
-            // Orario di partenza -> varchar(64) NOT NULL
-            $table->smallInteger('time_departures');
-            // Orario di arrivo -> smallint NULL UNSIGNED
-            $table->smallInteger('time_arrivals');
-            // Codice treno -> smallint NULL UNSIGNED
-            $table->smallInteger('train_num')->nullable()->unsigned();
-            // Numero Carrozze -> smallint NULL UNSIGNED
-            $table->smallInteger('num_train_carriagies')->nullable()->unsigned();
-            // In orario -> smallint NULL UNSIGNED
-            $table->smallInteger('on_time')->nullable()->unsigned();
-            // Cancellato -> smallint NULL UNSIGNED
-            $table->smallInteger('cancelled')->nullable()->unsigned();
+
+            // Orario di partenza -> time NOT NULL UNSIGNED
+            $table->time('time_departures', $precision = 0);
+
+            // Orario di arrivo -> time NOT NULL UNSIGNED
+            $table->time('time_arrivals', $precision = 0);
+
+            // Codice treno -> tinyint NOT NULL UNSIGNED
+            $table->strign('train_num')->unsigned();
+
+            // Numero Carrozze -> tinyint NULL UNSIGNED
+            $table->integer('num_train_carriagies')->unsigned();
+
+            // In orario -> boolean NOT NULL 
+            $table->boolean('on_time');
+
+            // Cancellato -> boolean NOT NULL 
+            $table->boolean('cancelled');
 
             // Crea 2 colonne: created_at e updated_at
             $table->timestamps();
