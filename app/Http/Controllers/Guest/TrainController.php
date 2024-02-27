5b8b23354c6b40ab240660f4615908b10ce57d13
martinaapalazzi <martinaapalazzi@gmail.com>
2024-02-27 15:40:08 +0100

    created Train Model

---
+++ app/Http/Controllers/Guest/TrainController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Guest;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+// Models
+use App\Models\Train;
+
+class TrainController extends Controller
+{
+    public function index()
+    {
+    
+    }
+
+    public function show($id)
+    {
+        
+    }  
+}
